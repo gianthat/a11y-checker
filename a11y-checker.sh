ccc2fee2b146ad211cd7f931432e594c5889bc32
Reid Burnett <reid.burnett@1905newmedia.com>
2023-11-12 12:32:30 -0600

    give user feedback on which domain is scanned, fix subpage report feedback

--- a11y-checker.sh
+++ a11y-checker.sh
@@ -34,11 +34,12 @@ process_domain() {
   echo "https://$domain" > "$temp_url_file"
 
   local log_file="logs/achecker-log-${sanitized_domain}-$CURRENT_DATETIME.txt"
+  echo "Scanning $original_domain..."
   npx achecker --outputFormat "$OUTPUT_FORMAT" --outputFolder "$report_dir" "$temp_url_file" >"$log_file" 2>&1
 
-  local report_file="${report_dir}/${sanitized_domain}.${OUTPUT_FORMAT}"
-  if [ -f "$report_file" ]; then
-    echo "Report generated for $original_domain: $report_file"
+  # Check for the existence of any report file in the directory
+  if compgen -G "${report_dir}/*" > /dev/null; then
+    echo "Report generated for $original_domain: $report_dir/"
   else
     echo "No report generated for $original_domain"
   fi
