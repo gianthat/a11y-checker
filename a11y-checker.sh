471c06af3c32273ad3da2092dba189231c94fb0c
Reid Burnett <reid.burnett@1905newmedia.com>
2023-11-11 22:14:13 -0600

    improve nesting of reports of similar domains

--- a11y-checker.sh
+++ a11y-checker.sh
@@ -16,14 +16,11 @@ done
 # Remove processed options from arguments
 shift $((OPTIND-1))
 
-# Script to run IBM Equal Access Accessibility Checker (achecker) on given domains
-
-# Function to check if a command exists
+# Check for required command: achecker
 command_exists() {
   command -v "$@" >/dev/null 2>&1
 }
 
-# Check for required command: achecker
 if ! command_exists achecker; then
   echo "Error: This script requires 'achecker'. Please install it first."
   exit 1
@@ -34,6 +31,9 @@ if [ "$#" -eq 0 ]; then
   echo "Usage: $0 [-f format] domain1[,domain2,...]"
   exit 1
 fi
+
+# Create logs and scans directories if they don't exist
+mkdir -p logs scans
 
 # Split the domain argument into an array
 IFS=',' read -ra DOMAINS <<< "$1"
@@ -53,33 +53,63 @@ for DOMAIN in "${DOMAINS[@]}"; do
   echo "$DOMAIN" >> "$URL_FILE"
 done
 
-# Run IBM Equal Access Accessibility Checker for all domains at once
-LOG_FILE="achecker-log-$CURRENT_DATETIME.txt"
-echo "Starting accessibility checks for all domains..."
+# Function to extract and sanitize base domain for directory naming
+get_base_domain() {
+  local url="$1"
+  local domain
 
-# Run achecker in the background
-npx achecker --outputFormat "$OUTPUT_FORMAT" --outputFolder "." "$URL_FILE" >"$LOG_FILE" 2>&1 &
+  # Replace trailing slash with an underscore if present
+  url=${url%/}
+  url=${url//\//_}
 
-# PID of the achecker process
-PID=$!
-
-# While achecker is running, echo "Still scanning..."
-while kill -0 $PID 2>/dev/null; do
-  echo "Still scanning, stand by..."
-  sleep 5
-done
-
-# Check and process reports for each domain
-for DOMAIN in "${DOMAINS[@]}"; do
-  if [ -f "${DOMAIN}.${OUTPUT_FORMAT}" ]; then
-    echo "✅ Completed scan for $DOMAIN. Report: a11y-check-${DOMAIN}-CURRENT_DATETIME.${OUTPUT_FORMAT}"
+  # Extract the domain from a URL if necessary
+  if [[ $url =~ ^https?://([^/]+) ]]; then
+    domain=${BASH_REMATCH[1]}
   else
-    echo "❌ Scanning $DOMAIN failed or no issues found."
+    domain=$url
+  fi
+
+  # Remove 'www.' if present
+  domain=${domain#www.}
+
+  # Extract the base domain
+  echo $domain | awk -F. '{print $(NF-1)"."$NF}'
+}
+
+# Process each domain
+for DOMAIN in "${DOMAINS[@]}"; do
+  BASE_DOMAIN=$(get_base_domain "$DOMAIN")
+
+  if [ -z "$BASE_DOMAIN" ]; then
+    echo "Error extracting base domain for $DOMAIN"
+    continue
+  fi
+
+  # Directory name based on base domain
+  BASE_DIR="scans/${BASE_DOMAIN}-$CURRENT_DATETIME"
+  mkdir -p "$BASE_DIR"
+
+  # Subdirectory for subpages
+  SUBPAGE_DIR=$(echo $DOMAIN | sed 's/^https:\/\/[^\/]*\///; s/[\/:]/_/g')
+  if [ "$SUBPAGE_DIR" != "" ]; then
+    mkdir -p "$BASE_DIR/$SUBPAGE_DIR"
+    REPORT_DIR="$BASE_DIR/$SUBPAGE_DIR"
+  else
+    REPORT_DIR="$BASE_DIR"
+  fi
+
+  # Run achecker for the domain
+  LOG_FILE="logs/achecker-log-$(echo $BASE_DOMAIN | awk -F/ '{print $1}')-$CURRENT_DATETIME.txt"
+  npx achecker --outputFormat "$OUTPUT_FORMAT" --outputFolder "$REPORT_DIR" "$URL_FILE" >"$LOG_FILE" 2>&1
+
+  # Rename the report file
+  if [ -f "$REPORT_DIR/${DOMAIN}.${OUTPUT_FORMAT}" ]; then
+    mv "$REPORT_DIR/${DOMAIN}.${OUTPUT_FORMAT}" "$REPORT_DIR/${DOMAIN//\//_}.${OUTPUT_FORMAT}"
+    echo "Report generated for $DOMAIN: $REPORT_DIR/${DOMAIN//\//_}.${OUTPUT_FORMAT}"
+  else
+    echo "No report generated for $DOMAIN"
   fi
 done
 
 # Clean up the URL file
-echo "Cleaning up the temp URL file."
 rm "$URL_FILE"
-
-echo "All scans completed. Check the logs and reports for details."
